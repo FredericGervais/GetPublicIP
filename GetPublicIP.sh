b6d1b266fbf24acdff17820302a1daba36fe077e
Frederic <frederic.gervais@outlook.com>
2019-12-10 18:28:24 -0500

    Update GetPublicIP.sh

--- GetPublicIP.sh
+++ GetPublicIP.sh
@@ -1,7 +1,7 @@
 AMAZON=$(curl -s https://checkip.amazonaws.com)
 CISCO=$(dig +short myip.opendns.com @resolver1.opendns.com)
 GOOGLE=$(dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}')
-DUCKDUCKGO=$(curl -s "http://api.duckduckgo.com/?q=ip&format=json" | grep -oP '\d+.\d+.\d+.\d+')
+DUCKDUCKGO=$(curl -s "https://api.duckduckgo.com/?q=ip&format=json" | grep -oP '\d+.\d+.\d+.\d+')
 ICANHAZIP=$(curl -s icanhazip.com)
 TORPROJECT=$(curl -s https://check.torproject.org/ | grep -oP '\d+.\d+.\d+.\d+')
 
